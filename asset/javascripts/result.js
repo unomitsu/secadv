e29468cdb3f016bf7e99acd5e246a7d9769aa957
unomitsu <s16t211@stu.kagawa-u.ac.jp>
2019-12-10 15:18:55 +0900

    解答後のまるとばつの表示ができた

--- asset/javascripts/result.js
+++ asset/javascripts/result.js
@@ -16,6 +16,15 @@ class SceneResult extends Scene {
         // 正答誤答フラグ
         this.correctFlag = false;
 
+        // ○と×を表示する画像
+        this.imgAnimation = document.createElement('img');
+
+        // ○と×の大きさ
+        this.imgAnimationSize = 0;
+
+        // カウンター
+        this.imgAnimationCounter = 0;
+
         // 親クラス系の各初期設定
         this.setDivScene();
         this.setDivPlayerData();
@@ -29,16 +38,28 @@ class SceneResult extends Scene {
 
     /* ----- 正答と誤答の表示 ----- */
     setAnimation() {
-        // 解答の確認
+        // 解答の確認とフラグの設定
         this.correctFlag = quizData["answer"][0] == playerAnswer ? this.correctFlag = true : this.correctFlag = false;
 
-        // テキストの設定とフラグの設定
+        // テキストの設定と画像の設定
         if (this.correctFlag) {
+            this.imgAnimation.src = "./asset/images/correct.png";
             this.setMainText(`正答です！あなたの回答は ${playerAnswer} でした。`);
         }
         else {
+            this.imgAnimation.src = "./asset/images/miss.png";
             this.setMainText(`誤答です。正答は ${quizData["answer"][0]} 。あんたの回答は ${playerAnswer} でした。`);
         }
+
+        // 正誤アニメーションの設定
+        this.imgAnimation.className = "animation";
+
+        // アニメーションの開始
+        this.imgAnimation_intervalEvent();
+
+        // ゲーム画面への追加
+        this.divScene.appendChild(this.imgAnimation);
+
     }
     /* ----- コンテニュー、ステージセレクト画面への移行イベントの設定 ----- */
     setMoveEvent() {
@@ -58,5 +79,24 @@ class SceneResult extends Scene {
         else {
             currentScene = new SceneContinue();
         }
+    }
+    /* ----- 一定時間ごとに画像の大きさを変更する ----- */
+    imgAnimation_intervalEvent() {
+        var id = setInterval(() => {
+            // 画像サイズとカウンターの加算
+            currentScene.imgAnimationSize += 1;
+            currentScene.imgAnimationCounter += 1;
+
+            // 画像の高さと位置の変更
+            currentScene.imgAnimation.style.top = gameHeight / 2 - currentScene.imgAnimationSize + "px";
+            currentScene.imgAnimation.style.height = 2 * currentScene.imgAnimationSize + "px";
+
+            console.log(currentScene.imgAnimation);
+
+            // 一定時間後処理終了
+            if (currentScene.imgAnimationCounter >= 150) {
+                clearInterval(id);
+            }
+        }, 100);
     }
 }
